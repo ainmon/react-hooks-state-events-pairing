87eb50cd0996c4ce641023bc78313bac29402d8a
Adam <inmon.adam@yahoo.com>
2021-12-11 17:52:30 -0600

    commit tree 4f772d68a56ef90ade1f5955a2034646c881a633

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
 import video from "../data/video.js";
+import Header from "./Header.js";
+import CommentList from "./CommentList.js";
+import { useState } from "react/cjs/react.development";
 
 function App() {
   console.log("Here's your data:", video);
+  
+  const [comment, setComments] = useState(true)
+
+  function hideComments(){
+    console.log('something!')
+    setComments((comment) => !comment)
+      
+ }
 
   return (
     <div className="App">
       <iframe
-        width="919"
+        width="925"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
+        src="https://www.youtube.com/embed/dpw9EHDh2bM"
       />
+      <Header video={video} handleCommentDisplay={hideComments} comment={comment}/>
+      <CommentList video={video} displayComment={comment} />
     </div>
+    
   );
 }
 
